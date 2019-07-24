4bdd0d172b4d26523c5c1f4411fddb7a26092897
product-team <product.team@groupdocs.com>
2019-07-24 08:00:37 -0500

    Updated sources

---
+++ src/test/java/com/groupdocs/cloud/comparison/api/Config.java
@@ -0,0 +1,11 @@
+package com.groupdocs.cloud.comparison.api;
+
+public class Config {
+    /*
+     * Get your AppSID and AppKey at https://dashboard.groupdocs.cloud/ (free
+     * registration is required)
+     */
+    public static String AppSID = "comparison.cloud";
+    public static String AppKey = "comparison.cloud";
+    public static String ApiBaseUrl = "https://api-qa.groupdocs.cloud";
+}
